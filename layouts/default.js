b9b71fe7156a0b013d0c535ed77d996412b71b0a
Andrew Cairns <andrew@acairns.co.uk>
2022-09-15 23:54:15 +0100

    Layout tweaks

--- layouts/default.js
+++ layouts/default.js
@@ -4,11 +4,11 @@ export default function Default({ title, children }) {
     return <div className='flex flex-col'>
         <Header />
         <div className='mt-8 flex flex-grow items-center justify-center'>
-            <div className="max-w-2xl p-10 sm:w-full">
-                <h2 className="text-6xl text-gray-700 font-bold">
+            <div className="prose prose-2xl max-w-2xl p-10 sm:w-full">
+                <h1 className="text-6xl text-gray-700 font-bold">
                     {title}
-                </h2>
-                <div className='prose prose-2xl mt-8'>
+                </h1>
+                <div className='mt-8'>
                     {children}
                 </div>
             </div>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,11 @@
 import md from 'markdown-it';
 import { listPosts, readPost } from '@/utils/posts';
+import Default from '../../layouts/default';
 
-export default function Post({ content, frontmatter }) {
-    return <div
-        className="prose lg:prose-xl"
-        dangerouslySetInnerHTML={{ __html: md().render(content) }} />;
+export default function Post({ content, ...frontmatter }) {
+    return <Default title={frontmatter.title}>
+        <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+    </Default>;
 };
 
 export async function getStaticPaths() {
