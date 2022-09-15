4fbb90535f21ff03377c656ea63ab1a23b0e670b
Andrew Cairns <andrew@acairns.co.uk>
2022-09-16 00:25:30 +0100

    Tweaking header

--- components/header.js
+++ components/header.js
@@ -1,24 +1,11 @@
 import Link from 'next/link';
 
-export default function Header()
-{
-    return <div className='flex items-center justify-center'>
-        <div className="sm:max-w-6xl p-10 w-full">
-            <div className='flex'>
-                <Link href='/'>
-                    <a>
-                        <img src="/andrew-cairns.png" alt="Andrew Cairns" style={{width: 33}} />
-                    </a>
-                </Link>
-                <ul className='flex flex-row space-x-4 text-sm font-bold flex-grow justify-end'>
-                    <li>
-                        <a href="https://twitter.com/andrewcairns" className='text-blue-700'>Contact me</a>
-                    </li>
-                    <li>
-                        <a href="https://trypatchwork.com" className='text-blue-700 border border-indigo-700 border-2 px-5 py-2 rounded-full hover:bg-indigo-700 hover:text-white'>Patchwork</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+export default function Header() {
+    return <div className='flex justify-center p-10'>
+        <Link href='/'>
+            <a>
+                <img src="/andrew-cairns.png" alt="Andrew Cairns" style={{ width: 33 }} />
+            </a>
+        </Link>
     </div>;
 }
--- layouts/default.js
+++ layouts/default.js
@@ -4,8 +4,8 @@ export default function Default({ title, children }) {
     return <div className='flex flex-col'>
         <Header />
         <div className='mt-8 flex flex-grow items-center justify-center'>
-            <div className="prose prose-2xl max-w-2xl p-10 sm:w-full">
-                <h1 className="text-6xl text-gray-700 font-bold">
+            <div className="prose prose-2xl prose-headings:text-gray-700 max-w-2xl p-10 sm:w-full">
+                <h1 className="text-6xl font-bold">
                     {title}
                 </h1>
                 <div className='mt-8'>
