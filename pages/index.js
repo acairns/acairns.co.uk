83583e474ca2560aa5a66c94f73ee5cc1bee23c4
Andrew Cairns <andrew@acairns.co.uk>
2022-09-16 00:00:54 +0100

    Squinty squinty squint

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,11 @@ export default function Home() {
                 My name is Andrew Cairns and I'm a Software Engineer living in Scotland. I've been building software
                 since Turbo Pascal on Amstrad. That's an old programming language no one writes anymore for an old computer no one uses anymore.
             </p>
-            <p className='prose-a:no-underline hover:prose-a:underline'>
+            <p className='prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded'>
                 I'm a Lead Back-End Engineer at {' '}
-                <a href="https://transfergo.com" target="_blank" className="p-1 rounded bg-yellow-300">TransferGo</a>.
+                <a href="https://transfergo.com" target="_blank" className="bg-yellow-300 -rotate-1">TransferGo</a>.
                 When I'm not working, you'll find me kayaking, coaching youth football or building {' '}
-                <a href="https://trypatchwork.com" target="_blank" className="p-1 rounded bg-indigo-600 text-white">Patchwork</a> {' '}
+                <a href="https://trypatchwork.com" target="_blank" className="bg-indigo-600 text-white rotate-2">Patchwork</a> {' '}
                 to help third-sector organisations with storytelling.
             </p>
             <p>
