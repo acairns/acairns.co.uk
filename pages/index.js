ec828a4c529c16236bceb932134326eb73258d34
Andrew Cairns <andrew@acairns.co.uk>
2023-12-09 16:08:25 +0000

    Tweak homepage

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,75 @@
-import Author from 'components/author';
-import Head from 'next/head'
-import Default from 'layouts/default';
-import { listPosts } from 'utils/posts';
-import Link from 'next/link';
+import Author from "components/author";
+import Head from "next/head";
+import Default from "layouts/default";
+import { listPosts } from "utils/posts";
+import Link from "next/link";
 
 export default function Home({ latestPosts }) {
-    return <>
-        <Head>
-            <meta key="description" name="description" content="My name is Andrew Cairns and I'm a Software Engineer living in Scotland. This is where I share my thoughts regarding software development." />
+  return (
+    <>
+      <Head>
+        <meta
+          key="description"
+          name="description"
+          content="My name is Andrew Cairns and I'm a Software Engineer living in Scotland. This is where I share my thoughts regarding software development."
+        />
 
-            <meta key="og:title" property="og:title" content="Andrew Cairns" />
-            <meta key="og:description" property="og:description" content="My name is Andrew Cairns and I'm a Software Engineer living in Scotland. This is where I share my thoughts regarding software development." />
-        </Head>
-        <Default title='Howdy!' narrow>
-            <Author />
-            <p>
-                My name is Andrew Cairns and I'm a Software Engineer living in Scotland. I've been building software since Turbo Pascal on Amstrad. That's an old programming language no one writes anymore for an old computer no one uses anymore.
-            </p>
-            <p className='prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded hover:prose-a:shadow-md'>
-                I work at {' '}
-                <a href="https://transfergo.com" target="_blank" className="bg-yellow-300 text-slate-700 -rotate-1">TransferGo</a> {' '}
-                building Compliance, Risk and Anti-money Laundering stuff.
-                I spend my spare time kayaking, coaching youth football or building {' '}
-                <a href="https://trypatchwork.com" target="_blank" className="bg-indigo-600 text-white rotate-2">Patchwork</a> {' '}
-                to help third-sector organisations with storytelling.
-            </p>
+        <meta key="og:title" property="og:title" content="Andrew Cairns" />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="My name is Andrew Cairns and I'm a Software Engineer living in Scotland. This is where I share my thoughts regarding software development."
+        />
+      </Head>
+      <Default title="Howdy!" narrow>
+        <Author />
+        <p>
+          My name is Andrew Cairns and I'm a Software Engineer and Content
+          Creator living in Scotland.
+        </p>
+        <p>
+          I've built software since Turbo Pascal on Amstrad - an old language no
+          one remembers for an old computer no one used.
+        </p>
+        <p className="prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded hover:prose-a:shadow-md">
+          I'm curently a software and content consultant through my company{" "}
+          <a
+            href="https://trypatchwork.com"
+            target="_blank"
+            className="bg-indigo-600 text-white rotate-2"
+          >
+            Patchwork
+          </a>
+          .
+        </p>
 
-            <p className='prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded hover:prose-a:shadow-md'>
-                Recently, I decided to move my writing to <a href="https://acairns.substack.com" target="_blank" className="bg-orange-600 text-white -rotate-2">Substack</a> {' '}
-                and start a <a href="https://www.youtube.com/@andrew.cairns" target="_blank" className="bg-red-600 text-white rotate-2">Devlog on YouTube</a>!
-                If you'd like to stay in the loop with my content - follow me there!
-            </p>
+        <p className="prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded hover:prose-a:shadow-md">
+          Previously, I've worked at places like{" "}
+          <a
+            href="https://transfergo.com"
+            target="_blank"
+            className="bg-yellow-300 text-slate-700 -rotate-1"
+          >
+            TransferGo
+          </a>{" "}
+          where I was building Compliance, Risk and Anti-money Laundering stuff.
+          I spend my spare time kayaking and coaching youth football.
+        </p>
 
-            {
-                /*
+        <p className="prose-a:no-underline hover:prose-a:underline prose-a:px-2 prose-a:inline-block prose-a:rounded hover:prose-a:shadow-md">
+          Recently, I decided to take{" "}
+          <a
+            href="https://www.youtube.com/@andrew.cairns"
+            target="_blank"
+            className="bg-red-600 text-white rotate-2"
+          >
+            YouTube
+          </a>{" "}
+          more seriously! If you'd like to stay in the loop with my content -
+          follow me there!
+        </p>
+
+        {/*
             <h2>Latest Articles</h2>
             <div className='space-y-4'>
                 {
@@ -55,17 +91,16 @@ export default function Home({ latestPosts }) {
                 }
             </div>
 
-    /**/
-            }
-
-        </Default>
-    </>;
+    /**/}
+      </Default>
+    </>
+  );
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            latestPosts: listPosts().slice(0, 3)
-        }
-    };
+  return {
+    props: {
+      latestPosts: listPosts().slice(0, 3),
+    },
+  };
 }
