fc9b8fdf985b6f823cfae5ac4e1d965af0777bae
Andrew Cairns <andrew@acairns.co.uk>
2024-02-02 15:57:32 +0000

    And again

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div className="bg-red-400">
       <h1>Welcome to Remix</h1>
-      <p>Trying again...</p>
+      <p>Trying again, and again...</p>
     </div>
   );
 }
