4567a690844154003764a08a2f11e961fe8a050e
Andrew Cairns <andrew@acairns.co.uk>
2024-02-06 15:57:08 +0000

    Mario is a Mario

---
+++ web/public/animations/loop-5177d8f0.js
@@ -0,0 +1,1 @@
+import{f as a,t as l}from"./Vector-861d74d7.js";a(i,l());function*i(t,r){for(let e=0;e<t;e++){const o=r(e);o?yield*o:yield}}export{i as l};
--- web/public/animations/sequence-b8ce4e26.js
+++
@@ -1,1 +0,0 @@
-import{f as r,w as t,t as i}from"./Vector-861d74d7.js";import{l as s}from"./makeScene2D-cf99b3aa.js";r(f,i());function*f(e,...o){for(const a of o)yield a,yield*t(e);yield*s(...o)}export{f as s};
