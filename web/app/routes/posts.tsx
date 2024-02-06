8b8ef74ecee48a9001828557f5375cfaea2d407e
Andrew Cairns <andrew@acairns.co.uk>
2024-02-06 14:23:07 +0000

    Icons and newsletter

--- web/app/routes/posts.tsx
+++ web/app/routes/posts.tsx
@@ -2,6 +2,11 @@ import {Outlet} from "@remix-run/react";
 
 export default function Posts() {
     return (
-        <Outlet />
+        <>
+            <Outlet />
+            <div className="bg-white">
+                <iframe className='w-full h-[800px]' src="https://acairns.substack.com/embed" frameBorder="0" scrolling="no" />
+            </div>
+        </>
     );
 }
