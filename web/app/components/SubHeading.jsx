c159681e874b3878a585222b562047ae4d8f6ade
Andrew Cairns <andrew@acairns.co.uk>
2024-02-07 17:57:07 +0000

    Fix anchor alignment

--- web/app/components/SubHeading.jsx
+++ web/app/components/SubHeading.jsx
@@ -11,7 +11,7 @@ export default function SubHeading({title}) {
 
     return (
         <h2 id={`${slug}`}>
-            <NavLink to={`#${slug}`} className="md:-ml-11 md:mr-4 mr-2 text-orange-500 no-underline">#</NavLink>
+            <NavLink to={`#${slug}`} className="md:-ml-[46px] md:mr-4 mr-2 text-orange-500 no-underline">#</NavLink>
             {title}
         </h2>
     );
