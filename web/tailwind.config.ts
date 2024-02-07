9e3502dc7ce6db8105caf8246b964d44322a43ef
Andrew Cairns <andrew@acairns.co.uk>
2024-02-07 17:00:04 +0000

    Auto linking headlines

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx,mdx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx,mdx}", "./vite.config.ts"],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
-  ],
+  ]
 } satisfies Config;
