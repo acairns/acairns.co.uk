9c61a729f07b953a082dbbebd60da42c49657465
Andrew Cairns <andrew@acairns.co.uk>
2024-02-09 14:07:10 +0000

    Conditional to ensure production env

--- web/app/components/website/Fathom.jsx
+++ web/app/components/website/Fathom.jsx
@@ -3,6 +3,10 @@ import { load } from 'fathom-client'
 import { useEffect } from 'react'
 
 const Fathom = () => {
+    if (process.env.NODE_ENV !== 'production') {
+        return null;
+    }
+
     useEffect(() => {
         load('QPZGXLRS', {
             // includedDomains: ['your.site.domain'],
